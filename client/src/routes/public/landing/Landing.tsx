9a6e5e7f8bbab11fe5fd026895f12fcfc84f8ace
Andrey Mironov <a.mironov@lucky-team.pro>
2024-03-30 01:08:53 +0300

    fix: check deploy

--- client/src/routes/public/landing/Landing.tsx
+++ client/src/routes/public/landing/Landing.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 export const Landing = () => {
   return (
     <div>
-      Landing <Link to={'site/1'}>link</Link>
+      Landing <Link to={'site/1'}>link</Link> <Link to="/login">login</Link>
     </div>
   )
 }
